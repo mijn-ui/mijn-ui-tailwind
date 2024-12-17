d18ca971b0d70f8c70e42ed1d74b3cdad833ad7a
HTLA380 <htetlin099798@gmail.com>
2024-12-17 20:00:24 +0630

    fix: incorrect sitemap url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { baseUrl, createMetadata } from "@/lib/metadata"
 import "./css/global.css"
 import { RootProvider } from "fumadocs-ui/provider"
 import { Inter } from "next/font/google"
@@ -5,6 +6,16 @@ import type { ReactNode } from "react"
 
 const inter = Inter({
   subsets: ["latin"],
+})
+
+export const metadata = createMetadata({
+  title: {
+    template: "%s | MijnUI",
+    default: "MijnUI",
+  },
+  description:
+    "MijnUI-Tailwind offers flexible, ready-to-use components for building marketing sites, dashboards, and e-commerce platforms.",
+  metadataBase: baseUrl,
 })
 
 export default function Layout({ children }: { children: ReactNode }) {
